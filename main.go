3effc60e3a1ed74c77d1c98cf0a3cf2efed424c0
Gregory Vorozhtsov <its.crowd@gmail.com>
2019-01-31 17:39:40 +0300

    fix

--- main.go
+++ main.go
@@ -10,16 +10,6 @@ import (
 	"github.com/micro/go-micro"
 )
 
-func createDummyData(repo Repository) {
-	defer repo.Close()
-	vessels := []*pb.Vessel{
-		{Id: "vessel001", Name: "Kane's Salty Secret", MaxWeight: 200000, Capacity: 500},
-	}
-	for _, v := range vessels {
-		repo.Create(v)
-	}
-}
-
 func main() {
 
 	host := os.Getenv("DB_HOST")
@@ -30,10 +20,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error connecting to datastore: %v", err)
 	}
-
-	repo := &VesselRepository{session.Copy()}
-
-	createDummyData(repo)
 
 	srv := micro.NewService(
 		micro.Name("go.micro.srv.vessel"),
